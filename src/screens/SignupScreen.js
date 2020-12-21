6af3091eee3bae65b3b91f8962ce2f894fe06c9e
matthewmjm <matthewmjm@yahoo.com>
2020-12-21 15:34:11 -0700

    style signup screen using react native elements and create a spacer component for margins

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
     return (
         <>
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-            <Button 
-                title="Go to Signin" 
-                onPress={() => navigation.navigate('Signin')} 
-            />
-            <Button 
-                title="Go to Main Flow"
-                onPress={() => navigation.navigate('mainFlow')}
-            />
+            <Spacer>
+                <Text h2>Sign Up for Tracker</Text>
+            </Spacer>
+            <Spacer>
+                <Input label="Email" />
+            </Spacer>
+            <Spacer>
+                <Input label="Password" />
+            </Spacer>
+            <Spacer>
+                <Button title="Sign Up"/>
+            </Spacer>
         </>
     )
 };
