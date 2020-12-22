990a76d9e1f33be6d3ffb2817b07e6451e52db4d
matthewmjm <matthewmjm@yahoo.com>
2020-12-22 13:03:29 -0700

    using components for SignUp, configured SignIn screen except for auth

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>SigninScreen</Text>
+        <View style={styles.container}>
+            <AuthForm 
+                headerText="Sign In to Your Account"
+                errorMessage=""
+                onSubmit={() => {}}
+                submitButtonText="Sign In"
+            />
+            <NavLink 
+                text="Don't have an account? Sign up instead"
+                routeName="Signup"
+            /> 
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = {
+    header: null
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+    },
+});
 
 export default SigninScreen;
