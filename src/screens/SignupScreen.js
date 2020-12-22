24728b66a3b9f3803c2fe5433319433dcbb92f9e
matthewmjm <matthewmjm@yahoo.com>
2020-12-22 16:16:17 -0700

    update for automatic signin if token exists

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';  
@@ -6,7 +6,11 @@ import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
-    const { state, signup, clearErrorMessage } = useContext(AuthContext);
+    const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext);
+
+    useEffect(() => {
+        tryLocalSignin();
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -25,11 +29,6 @@ const SignupScreen = ({ navigation }) => {
     );
 };
 
-SignupScreen.navigationOptions = () => {
-    return {
-        header: () => false,
-    };
-};
 
 const styles = StyleSheet.create({
     container: {
