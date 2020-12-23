2fc31495a877b3aa7c82bd5d936853ce87a3c264
matthewmjm <matthewmjm@yahoo.com>
2020-12-22 22:02:06 -0700

    add location functionality and creating fake user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import '../_mockLocation';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 
 
-const TrackCreateScreen = ({ navigation }) => {
+const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+                console.log(location);
+            });
+        } catch (e) {
+            setErr(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    });
+
     return (
-        <SafeAreaView>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
-    )
+    );
 };
 
 const styles = StyleSheet.create({});
