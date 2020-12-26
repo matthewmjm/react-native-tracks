637c18fb28374666d31b066e3ebe45cea4e3bc85
matthewmjm <matthewmjm@yahoo.com>
2020-12-26 11:09:54 -0700

    allow for recording of tracking when switch screens

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,46 +7,39 @@ import {
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null);
-    const [subscriber, setSubscriber] = useState(null);
+    // const [subscriber, setSubscriber] = useState(null);
 
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            const sub = await watchPositionAsync(
-                {
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000,
-                    distanceInterval: 10,
-                }, 
-                callback
-            );
-            setSubscriber(sub)
-            // const { granted } = await requestPermissionsAsync();
-            // if (!granted) {
-            //     throw new Error('Location permission not granted');
-            // }
-            // await watchPositionAsync(
-            //     {
-            //         accuracy: Accuracy.BestForNavigation,
-            //         timeInterval: 1000,
-            //         distanceInterval: 10,
-            //     }, 
-            //     (location) => {
-            //         console.log(location);
-            //         addLocation(location);
-            //     }
-            // );
-        } catch (e) {
-            setErr(e);
-        }
-    };
+
 
     useEffect(() => {
+        let subscriber;
+        const startWatching = async () => {
+            try {
+                const { granted } = await requestPermissionsAsync();
+                if (!granted) {
+                    throw new Error('Location permission not granted');
+                }
+                subscriber = await watchPositionAsync(
+                    {
+                        accuracy: Accuracy.BestForNavigation,
+                        timeInterval: 1000,
+                        distanceInterval: 10,
+                    }, 
+                    callback
+                );
+                // setSubscriber(sub)
+            } catch (e) {
+                setErr(e);
+            }
+        };
+
         if (shouldTrack) {
             startWatching();
         } else {
-            subscriber.remove();
-            setSubscriber(null);
+            if (subscriber) {
+                subscriber.remove();
+            }
+            subscriber = null;
         }
 
         return () => {
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -9,11 +9,15 @@ import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-    const { state, addLocation } = useContext(LocationContext);
+    const { 
+        state: { recording }, 
+        addLocation 
+    } = useContext(LocationContext);
+
     const callback = useCallback(location => {
-        addLocation(location, state.recording);
-    }, [state.recording]);
-    const [err] = useLocation(isFocused, callback);
+        addLocation(location, recording);
+    }, [recording]);
+    const [err] = useLocation(isFocused || recording, callback);
 
     console.log(isFocused);
 
@@ -21,7 +25,6 @@ const TrackCreateScreen = ({ isFocused }) => {
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
             <Map />
-            {/* <NavigationEvents onWillBlur={() => console.log("LeAvInG!!") } /> */}
             {err ? <Text>Please enable location services</Text> : null}
             <TrackForm />
         </SafeAreaView>
