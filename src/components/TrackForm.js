62ba31f3cc8c626ed99c56a54be41f2d72722c09
matthewmjm <matthewmjm@yahoo.com>
2020-12-26 11:59:20 -0700

    coordinate between the Track context and the Location context

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
     const { state: { name, recording, locations }, 
         startRecording, 
         stopRecording, changeName } 
         = useContext(LocationContext);
+        const [saveTrack] = useSaveTrack();
 
     console.log(locations.length);
 
@@ -26,6 +28,13 @@ const TrackForm = () => {
                     : <Button title="Start Recording" onPress={startRecording} />
                 }
                 
+            </Spacer>
+            <Spacer>
+            {
+                !recording && locations.length
+                ? <Button title="Save Recording" onPress={saveTrack} />
+                : null
+            }
             </Spacer>
         </>
     );
