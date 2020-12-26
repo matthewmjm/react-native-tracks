0c78409e4269beefb3fddce76d3ef96d364a9c93
matthewmjm <matthewmjm@yahoo.com>
2020-12-26 16:34:53 -0700

    navigation to saved track

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
-import { SafeAreaView } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext);
+
     return (
-        <SafeAreaView forceInset={{ top: 'always' }}>
-            <Text>TrackListScreen</Text>
-            <Button 
-                title="Go to Track Detail"
-                onPress={() => navigation.navigate('TrackDetail')}
-            />
-        </SafeAreaView>
-    )
+        <>
+            <NavigationEvents onWillFocus={fetchTracks} />
+            <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+            <FlatList 
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() =>
+                                navigation.navigate('TrackDetail', { _id: item._id })
+                            }
+                        >
+                            <ListItem style={styles.list} chevron={true} title={item.name}/>
+                        </TouchableOpacity>
+                    );
+                }}
+            /> 
+        </>
+    );
 };
 
-TrackListScreen.navigationOptions = {
-    header: () => false,
-};
+// TrackListScreen.navigationOptions = {
+//     header: () => false,
+// };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    list: {
+        color: 'red'
+    }
+});
 
 export default TrackListScreen;
